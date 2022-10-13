fcf4dc8a908964288a1771217bb711f0bd986f68
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-10-13 19:53:25 +0300

    ...

---
+++ Restaurant.Notification/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Restaurant.Notification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .ConfigureServices((hostContext, services) =>
+               {
+                   services.AddHostedService<Worker>();
+               });
+    }
+}
