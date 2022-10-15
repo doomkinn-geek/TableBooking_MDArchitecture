6d5deff33a5d76366efc05e1f8a1c3362ebc6f91
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-10-15 21:52:26 +0300

    методичка

---
+++ Messaging/Producer.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public class Producer
+    {
+        private readonly string _queueName;
+        private readonly string _hostName;
+
+        public Producer(string queueName, string hostName)
+        {
+            _queueName = queueName;
+            _hostName = hostName;
+        }
+
+        public void Send(string message)
+        {
+            var factory = new ConnectionFactory() { HostName = _hostName };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(
+                "direct_exchange",
+                "direct",
+                false,
+                false,
+                null
+            );
+
+            var body = Encoding.UTF8.GetBytes(message); // формируем тело сообщения для отправки
+
+            channel.BasicPublish(exchange: "direct_exchange",
+                routingKey: _queueName,
+                basicProperties: null,
+                body: body); //отправляем сообщение
+        }
+    }
+}
