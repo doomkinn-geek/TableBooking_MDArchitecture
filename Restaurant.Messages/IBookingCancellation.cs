1d6bcfffef09d3edf0dc3895c5a2a6e67e21b277
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-10-20 19:48:20 +0300

    inited

---
+++ Restaurant.Messages/IBookingCancellation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public interface IBookingCancellation
+{
+    public Guid OrderId { get; }
+}
+
+public class BookingCancellation : IBookingCancellation
+{
+    public BookingCancellation(Guid orderId)
+    {
+        OrderId = orderId;
+    }
+
+    public Guid OrderId { get; }
+}
